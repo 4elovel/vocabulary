bf6e11bf3d2dd44f9d58dd642abca909e48b711c
4elovel <marcynakvova007@gmail.com>
2022-11-10 19:17:08 +0300

    Добавьте файлы проекта.

---
+++ map/map.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	multimap <string, string> mp{
+		{"always","завжди"},
+		{"work","робота"},
+		{"never","нiколи"},
+		{"sometimes","деколи"},
+		{"maybe","можливо"},
+		{"wear","одягати"},
+		{"cheap","дешевий"},
+		{"rock","камiнь"},
+		{"pen","ручка"},
+		{"pencil","олiвець"}
+	};
+
+	for ( auto i : mp)
+	{
+		cout << i.first << " " << i.second << endl;
+	}
+
+}
